3bcd2950f277e5f4101cfe50843acda973e61fbc
amanda-santos <amandasf0200@gmail.com>
2023-11-28 21:16:07 -0300

    refactor: ♻️ authenticate controller

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -8,15 +8,23 @@ import { CreateAccountController } from './controllers/create-account.controller
 import { CreateQuestionController } from './controllers/create-question.controller';
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller';
 import { DatabaseModule } from '../database/database.module';
+import { CryptographyModule } from '../cryptography/cryptography.module';
+import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student';
+import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, CryptographyModule],
   controllers: [
     CreateAccountController,
     AuthenticateController,
     CreateQuestionController,
     FetchRecentQuestionsController,
   ],
-  providers: [CreateQuestionUseCase, FetchRecentQuestionsUseCase],
+  providers: [
+    CreateQuestionUseCase,
+    FetchRecentQuestionsUseCase,
+    RegisterStudentUseCase,
+    AuthenticateStudentUseCase,
+  ],
 })
 export class HttpModule {}
