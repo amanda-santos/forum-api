a82408759f6796e4fe3658cd7a20822f7f3150aa
amanda-santos <amandasf0200@gmail.com>
2023-12-01 19:52:13 -0300

    refactor: ♻️ E2E tests

--- test/factories/make-answer-comment.ts
+++ test/factories/make-answer-comment.ts
@@ -1,4 +1,5 @@
 import { faker } from '@faker-js/faker';
+import { Injectable } from '@nestjs/common';
 
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 
@@ -6,6 +7,8 @@ import {
   AnswerComment,
   AnswerCommentProps,
 } from '@/domain/forum/enterprise/entities/answer-comment';
+import { PrismaService } from '@/infra/database/prisma/prisma.service';
+import { PrismaAnswerCommentMapper } from '@/infra/database/prisma/mappers/prisma-answer-comment-mapper';
 
 export function makeAnswerComment(
   override: Partial<AnswerCommentProps> = {},
@@ -22,4 +25,21 @@ export function makeAnswerComment(
   );
 
   return answer;
+}
+
+@Injectable()
+export class AnswerCommentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaAnswerComment(
+    data: Partial<AnswerCommentProps> = {},
+  ): Promise<AnswerComment> {
+    const answerComment = makeAnswerComment(data);
+
+    await this.prisma.comment.create({
+      data: PrismaAnswerCommentMapper.toPrisma(answerComment),
+    });
+
+    return answerComment;
+  }
 }
